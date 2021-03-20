7bb8c48915561d65a9a022393352392743c757cc
Joel Louis <53653635+joellui@users.noreply.github.com>
2021-03-20 17:58:01 +0530

    Update invoice.js

--- public/classes/invoice.js
+++ public/classes/invoice.js
@@ -1,18 +1,18 @@
-//class
-export class Invoice {
-    //readonly client: string;
-    //private details: string;
-    //public amount: number;
-    constructor(//c: string,d:string,a:number
-    client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-        //this.client = c;
-        //this.details = d;
-        //this.amount = a;
-    }
-    format() {
-        return `${this.client} owes £${this.amount} for ${this.details}`;
-    }
-}
+// //class
+// export class Invoice {
+//     //readonly client: string;
+//     //private details: string;
+//     //public amount: number;
+//     constructor(//c: string,d:string,a:number
+//     client, details, amount) {
+//         this.client = client;
+//         this.details = details;
+//         this.amount = amount;
+//         //this.client = c;
+//         //this.details = d;
+//         //this.amount = a;
+//     }
+//     format() {
+//         return `${this.client} owes £${this.amount} for ${this.details}`;
+//     }
+// }
